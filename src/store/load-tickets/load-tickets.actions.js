5b90f58bae32f48a38304d67ddba3aa9d8a170a2
Rustam <isaev.rustam.a@gmail.com>
2022-12-18 22:47:55 +0300

    deleted cl

--- src/store/load-tickets/load-tickets.actions.js
+++ src/store/load-tickets/load-tickets.actions.js
@@ -1,10 +1,7 @@
 import AviaSalesApi from 'services/avia-sales-api/avia-sales-api';
 import { Loading } from 'store/types.actions';
 
-const start = (tickets) => {
-  console.log(tickets);
-  return { type: Loading.DOWNLOAD_START, tickets, loading: false, loadingAll: [true, 0] };
-};
+const start = (tickets) => ({ type: Loading.DOWNLOAD_START, tickets, loading: false, loadingAll: [true, 0] });
 const load = (tickets, load) => ({ type: Loading.LOADING, tickets, loading: false, loadingAll: [true, load] });
 const end = (tickets) => ({ type: Loading.DOWNLOAD_END, tickets, loading: false, loadingAll: [false, 100] });
 
